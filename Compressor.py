995438bcb9d663dfceee4d919812fed05ef8160f
dvhh <dvhh@users.noreply.github.com>
2017-11-01 11:05:15 +0900

    check builtin module dependency before adding the decompressor

--- Compressor.py
+++ Compressor.py
@@ -3,22 +3,40 @@
 import sublime
 import sublime_plugin
 
-import gzip
+''' 
+# header references
 
-compression_formats = {
-    '.gz': gzip.open,
-}
+- [gzip](https://tools.ietf.org/html/rfc1952)
+- [bzip2](https://en.wikipedia.org/wiki/Bzip2#File_format)
+- [xz](https://tukaani.org/xz/format.html)
 
-if sublime.version() >= '3114' :
-    import bz2
-    compression_formats['.bz2'] = bz2.open
+'''
+compression_modules = [
+    { 'module' : 'gzip', 'depend' : 'zlib',  'extension' : '.gz', 'header':'\x1f\x8b' },
+    # since build 3114
+    { 'module' : 'bz2',  'depend' : '_bz2',  'extension' : '.bz2', 'header' : 'BZ'},
+    # future proof 20171031
+    { 'module' : 'lzma', 'depend' : '_lzma', 'extension' : '.xz', 'header' : '\xFD7zXZ\x00'} 
+]
+
+compression_formats = {}
+
+import sys
+import importlib
+
+for compression_module in compression_modules :
+    dependendy = compression_module['depend'];
+    if dependendy in sys.builtin_module_names :
+        module    = compression_module['module']
+        extension = compression_module['extension']
+        decompressor = importlib.import_module(module);
+        compression_formats[extension] = decompressor.open
 
 def get_decompressor_by_filename(filename):
     for suffix, decompressor in compression_formats.items():
         if filename.endswith(suffix):
             return suffix, decompressor
     return None, None
-
 
 class DecompressFileCommand(sublime_plugin.TextCommand):
     def run(self, edit, filename=None, suffix=None, decompressor=None):
@@ -45,7 +63,6 @@ class DecompressFileCommand(sublime_plugin.TextCommand):
                 pos += view.insert(edit, pos, line.decode('utf-8'))
 
         view.set_read_only(True)
-
 
 class OpenCompressedFile(sublime_plugin.EventListener):
     def on_load(self, view):
